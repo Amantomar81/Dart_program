c7e3199b7e95e7c1795c6984a26e9943ed930237
Amantomar81 <tg81tomar@gmail.com>
2022-06-08 02:14:35 +0530

    update

--- .idea/10.Functions.dart
+++ .idea/10.Functions.dart
@@ -1,4 +1,12 @@
+//Functions are of two types
+//1. System defined Function
+// eg: main(), print(), write()
+//2. user defined | custom function
 
+//Function having two parts
+//1. Function Defination: What are tasks performed by the Function
+// Preferlly we put function defination to be outside of main()
+//  2. Function Calling: Call the Function that is already defined
 
 main(){
 PrintSomething();
@@ -8,7 +16,7 @@ PrintSomething();
 PrintSomething(){
   print("This is user function");
   print("you can call thi function anywhere, anytime inside main function");
-
+  print("---------------------");
 }
 
 //There are various variant of a function
@@ -23,4 +31,3 @@ category of function in new programming language
 3. Anonymous Function
  */
 
-// when we call a function inside another function is known as
